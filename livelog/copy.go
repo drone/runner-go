0a95ecec9830c0cd9d0092927b0defe359ad3a35
Eoin McAfee <eoin.mcafee@harness.io>
2021-11-03 11:37:28 +0000

    add support for kubernetes runner

--- livelog/copy.go
+++ livelog/copy.go
@@ -6,8 +6,13 @@ package livelog
 
 import (
 	"bufio"
+	"encoding/base64"
 	"io"
+	"io/ioutil"
+	"regexp"
 )
+
+var re = regexp.MustCompile("#((.*?)#)")
 
 // Copy copies from src to dst and removes until either EOF
 // is reached on src or an error occurs.
@@ -15,6 +20,15 @@ func Copy(dst io.Writer, src io.ReadCloser) error {
 	r := bufio.NewReader(src)
 	for {
 		bytes, err := r.ReadBytes('\n')
+		// check logs for card data
+		card := re.FindStringSubmatch(string(bytes))
+		if card != nil {
+			data, err := base64.StdEncoding.DecodeString(card[len(card)-1:][0])
+			if err == nil {
+				_ = ioutil.WriteFile("/tmp/card.json", data, 0644)
+			}
+			continue
+		}
 		if _, err := dst.Write(bytes); err != nil {
 			return err
 		}
